64ec3d57c46d8641103a6f94ea802801d06c9ff6
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2023-02-15 18:13:13 +0530

    [Shubham] Added. Using WebSocket, Connected Multiple clients with server for chat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
+from models import manager
+
 
 app = FastAPI()
 
@@ -12,9 +14,14 @@ async def get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
