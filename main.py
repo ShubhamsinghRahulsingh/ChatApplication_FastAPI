e955673df3e729cc8dae48a4dbb67f8d8d2a9dce
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2023-02-16 09:51:55 +0530

    [Shubham] Added. Taking Username as Client_Id for connection

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ app = FastAPI()
 templates = Jinja2Templates(directory="Templates")
 
 
-@app.get("/")
-async def get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/{client_id}")
+async def get(client_id: str, request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "client_id": client_id})
 
 
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"Client {client_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast(f"Client {client_id} left the chat")
--- models.py
+++ models.py
@@ -21,3 +21,4 @@ class ConnectionManager:
 
 
 manager = ConnectionManager()
+
